f621b5e7708d649cbbfb3361488845d5f590527c
professorwade <121523833+professorwade@users.noreply.github.com>
2025-04-09 08:12:02 -0400

    Added interface usage examples

---
+++ Interfaces/src/Measurables.java
@@ -0,0 +1,18 @@
+public class Measurables
+{
+    /**
+     Swaps the values with the smallest and largest measure.
+     @param objects an array of objects of a class that implements the
+     Measurable interface.
+
+     Provide a method that swaps the values with the smallest and largest measure in an array of objects
+     of a class that implements the Measurable interface.
+
+     */
+    public static void swapMinAndMax(Measurable[] objects)
+    {
+
+        /* Your code goes here */
+
+    }
+}
---
+++ Interfaces/src/Moveable.java
@@ -0,0 +1,11 @@
+/**
+ Describes any class whose objects can be moved.
+ */
+public interface Moveable
+{
+    /**
+     Moves this object by a given amount.
+     @param amount the amount by which to move the object
+     */
+    void move(double amount);
+}
